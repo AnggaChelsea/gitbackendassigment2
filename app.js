1af170b9da0c4f2040b62a75a6ac10aba00172f3
Angga Chelsea <muhidabdul168@gmail.com>
2020-09-30 14:31:06 +0700

    ini mulai dari app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongooseConnect = require('./config/mongoose');
+const routes = require('./routes');
+
+const app = express();
+const port = 4000;
+
+mongooseConnect();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`App runs on http://localhost:${port}`);
+});
